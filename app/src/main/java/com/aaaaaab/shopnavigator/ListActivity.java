d0be30156adac4ff84baef3c1363bc19c8c74905
Jan Steinke <jan.steinke@gmail.com>
2016-09-18 01:34:07 +0200

    send user requests to siroop API and display parsed results on activity

--- app/src/main/java/com/aaaaaab/shopnavigator/ListActivity.java
+++ app/src/main/java/com/aaaaaab/shopnavigator/ListActivity.java
@@ -5,19 +5,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class ListActivity extends AppCompatActivity implements TextWatcher {
+public class ListActivity extends AppCompatActivity {
     private EditText editText;
     private ListView listView;
     private ArrayAdapter listViewAdapter;
     private ArrayList<Editable> list;
+    private RequestQueue queue;
+
+    String url ="https://api.siroop.ch/product/search/?query=";
+    String limit = "&limit=10";
+    String apiKey = "&apikey=f246b0749fa7484e81d387a471ad67aa";
+
 
     //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
     ArrayList<String> listItems=new ArrayList<String>();
@@ -29,13 +47,14 @@ public class ListActivity extends AppCompatActivity implements TextWatcher {
 
         //Create an EditText widget and add the watcher
         editText = (EditText) findViewById(R.id.text_search);
-        editText.addTextChangedListener(this);
 
         list = new ArrayList<>();
         listViewAdapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, list);
         listView = (ListView) findViewById(R.id.listView2);
         listView.setAdapter(listViewAdapter);
+
+        queue = Volley.newRequestQueue(this);
     }
 
 
@@ -45,19 +64,42 @@ public class ListActivity extends AppCompatActivity implements TextWatcher {
         startActivity(intent);
     }
 
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    public void sendRequest(View view) {
+        listViewAdapter.clear();
+        // assemble reqUrl to receive JSON obj
+        String reqUrl = url + editText.getText() + limit + apiKey;
 
-    }
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, reqUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-        CharSequence text = s;
-        listViewAdapter.add(text);
-    }
+                        //parse response JSON obj
+                        JSONArray jsonArray = null;
+                        String name = null;
+                        try {
+                            jsonArray = new JSONArray(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
-    @Override
-    public void afterTextChanged(Editable s) {
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            try {
+                                listViewAdapter.add(jsonArray.getJSONObject(i).getString("name"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
 
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                listViewAdapter.clear();
+                listViewAdapter.add("Sorry, please try again.");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
