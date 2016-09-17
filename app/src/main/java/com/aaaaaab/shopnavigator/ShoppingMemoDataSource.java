f3e10335e883a1e20cf6f5f89701684bcd8f561a
Patrick <paddy@patrick-oschatzs-macbook-pro.local>
2016-09-18 01:33:24 +0200

    Final version of database including delete function

--- app/src/main/java/com/aaaaaab/shopnavigator/ShoppingMemoDataSource.java
+++ app/src/main/java/com/aaaaaab/shopnavigator/ShoppingMemoDataSource.java
@@ -6,7 +6,10 @@ package com.aaaaaab.shopnavigator;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
-
+import android.content.ContentValues;
+import android.database.Cursor;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingMemoDataSource {
 
@@ -14,6 +17,12 @@ public class ShoppingMemoDataSource {
 
     private SQLiteDatabase database;
     private ShoppingMemoDbHelper dbHelper;
+
+    private String[] columns = {
+            ShoppingMemoDbHelper.COLUMN_ID,
+            ShoppingMemoDbHelper.COLUMN_PRODUCT,
+            ShoppingMemoDbHelper.COLUMN_QUANTITY
+    };
 
 
     public ShoppingMemoDataSource(Context context) {
@@ -30,5 +39,70 @@ public class ShoppingMemoDataSource {
     public void close() {
         dbHelper.close();
         Log.d(LOG_TAG, "Database was closed with DbHelper.");
+    }
+
+    public ShoppingMemo createShoppingMemo(String product, int quantity) {
+        ContentValues values = new ContentValues();
+        values.put(ShoppingMemoDbHelper.COLUMN_PRODUCT, product);
+        values.put(ShoppingMemoDbHelper.COLUMN_QUANTITY, quantity);
+
+        long insertId = database.insert(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST, null, values);
+
+        Cursor cursor = database.query(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,
+                columns, ShoppingMemoDbHelper.COLUMN_ID + "=" + insertId,
+                null, null, null, null);
+
+        cursor.moveToFirst();
+        ShoppingMemo shoppingMemo = cursorToShoppingMemo(cursor);
+        cursor.close();
+
+        return shoppingMemo;
+    }
+
+    private ShoppingMemo cursorToShoppingMemo(Cursor cursor) {
+        int idIndex = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_ID);
+        int idProduct = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_PRODUCT);
+        int idQuantity = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_QUANTITY);
+
+        String product = cursor.getString(idProduct);
+        int quantity = cursor.getInt(idQuantity);
+        long id = cursor.getLong(idIndex);
+
+        ShoppingMemo shoppingMemo = new ShoppingMemo(product, quantity, id);
+
+        return shoppingMemo;
+    }
+
+    public List<ShoppingMemo> getAllShoppingMemos() {
+        List<ShoppingMemo> shoppingMemoList = new ArrayList<>();
+
+        Cursor cursor = database.query(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,
+                columns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        ShoppingMemo shoppingMemo;
+
+        while(!cursor.isAfterLast()) {
+            shoppingMemo = cursorToShoppingMemo(cursor);
+            shoppingMemoList.add(shoppingMemo);
+            Log.d(LOG_TAG, "ID: " + shoppingMemo.getId() + ", Content: " + shoppingMemo.toString());
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        return shoppingMemoList;
+    }
+
+    public void deleteShoppingMemo(ShoppingMemo shoppingMemo) {
+        long id = shoppingMemo.getId();
+
+        database.delete(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,
+                ShoppingMemoDbHelper.COLUMN_ID + "=" + id,
+                null);
+
+        Log.d(LOG_TAG, "Entry deleted! ID: " + id + " Content: " + shoppingMemo.toString());
+
+
     }
 }
